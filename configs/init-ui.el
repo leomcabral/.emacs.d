107c7e6993d184de96a679b6bbf01e492a80760f
Leonardo Cabral <leomcabral@gmail.com>
2019-05-31 17:47:36 -0300

    remove scrollbar

--- configs/init-ui.el
+++ configs/init-ui.el
@@ -1,8 +1,9 @@
 (use-package zenburn-theme
   :init
-  (load-theme 'zenburn t)
-  :config
-  (tool-bar-mode -1))
+  (load-theme 'zenburn t))
+
+(tool-bar-mode -1)
+(toggle-scroll-bar -1)
 
 ;; Set emacs frame size
 (setq initial-frame-alist '((width . 180) (height . 60)))
